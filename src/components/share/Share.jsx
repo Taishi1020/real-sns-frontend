89045034d8dfd97c15b3436fa2cf261c5013ee89
Taishi1020 <tai.yakiman1020@gmail.com>
2022-09-26 13:48:23 +0900

    upd: Share.jsxのHTML作成

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,8 +1,46 @@
 import React from "react"
 import "./sharec.css"
+import {Analytics, Face, Gif, Image} from "@mui/icons-material";
 
 export const Share = () => {
     return (
-        <div>Share</div>
+        <div className="share">
+           <div className="shareWrapper">
+               <div className="shareTop">
+                   <img
+                       src="/assets/person/1.jpeg"
+                       alt=""
+                       className="shareProfileImg"
+                   />
+                   <input
+                       type="text"
+                       className="shareInput"
+                       placeholder="今何しているの？"
+                   />
+                   <hr className="shareHr"/>
+               </div>
+               <div className="shareButtons">
+                   <div className="shareOptions">
+                       <div className="shareOption">
+                           <Image className="shareIcon"/>
+                           <span className="shareOptionText">写真</span>
+                       </div>
+                       <div className="shareOption">
+                           <Gif className="shareIcon"/>
+                           <span className="shareOptionText">GIF</span>
+                       </div>
+                       <div className="shareOption">
+                           <Face className="shareIcon"/>
+                           <span className="shareOptionText">気持ち</span>
+                       </div>
+                       <div className="shareOption">
+                           <Analytics className="shareIcon"/>
+                           <span className="shareOptionText">投票</span>
+                       </div>
+                   </div>
+                   <button className="shareButton">投稿</button>
+               </div>
+           </div>
+        </div>
     )
 }

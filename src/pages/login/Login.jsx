f071dea76253a25f532bb9777bd330020d90e1fd
Taishi1020 <tai.yakiman1020@gmail.com>
2022-10-02 08:53:09 +0900

    upd:　login画面の左側のcssを適用

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,15 @@
+import "./Login.css"
+
+export const Login = () => {
+    return (
+        <div className="login">
+            <div className="loginWrapper">
+                <div className="loginLeft">
+                    <h3 className="loginLogo">Real SNS</h3>
+                    <span className="loginDesc">本格的なSNSを、自分の手で</span>
+                </div>
+                <div className="loginRight">右側</div>
+            </div>
+        </div>
+    )
+}

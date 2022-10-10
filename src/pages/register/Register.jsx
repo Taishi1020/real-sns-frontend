e46430de92e801b2794f8478b9de7c4cc846548c
Taishi1020 <tai.yakiman1020@gmail.com>
2022-10-10 23:11:31 +0900

    upd:　ユーザー新規登録実装

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,7 @@
 import "./Register.css"
 import {loginCall} from '../../actionCalls'
 import {useRef} from 'react'
-
+import axios from 'axios'
 
 
 export const Register = () => {
@@ -10,8 +10,26 @@ export const Register = () => {
     const password = useRef();
     const passwordConfirmation = useRef();
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
+
+        //パスワードと確認用のパスワードがあっているかどうか確認する処理
+        if(password.current.value !== passwordConfirmation.current.value) {
+            passwordConfirmation.current.setCustomValidity("パスワードが違います")
+        }else {
+            try{
+                //registerAPI
+                const user = {
+                    username: username.current.value,
+                    email: email.current.value,
+                    password: password.current.value,
+                };
+                await axios.post("auth/register", user)
+            }catch (err){
+                console.log(err)
+            }
+
+        }
     }
 
     return (
@@ -29,12 +47,14 @@ export const Register = () => {
                             className="loginInput"
                             placeholder="ユーザー名"
                             required
+                            ref={username}
                         />
                         <input
                             type="email"
                             className="loginInput"
                             placeholder="Eメール"
                             required
+                            ref={email}
                         />
                         <input
                             type="password"
@@ -42,6 +62,7 @@ export const Register = () => {
                             placeholder="パスワード"
                             required
                             minLength="6"
+                            ref={password}
                         />
                         <input
                             type="password"
@@ -49,6 +70,7 @@ export const Register = () => {
                             placeholder="確認用パスワード"
                             required
                             minLength="6"
+                            ref={passwordConfirmation}
                         />
                         <button className="loginButton" type="submit">サインアップ</button>
                         <button className="loginRegisterButton">ログイン</button>

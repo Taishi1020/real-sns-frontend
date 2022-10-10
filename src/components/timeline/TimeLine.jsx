684f7c250da3a19fbf676de05505409135b63d51
Taishi1020 <tai.yakiman1020@gmail.com>
2022-10-11 08:08:57 +0900

    upd:　ログイン中のユーザーに対してタイムラインを表示させる設定

--- src/components/timeline/TimeLine.jsx
+++ src/components/timeline/TimeLine.jsx
@@ -1,22 +1,24 @@
-import React, {useEffect, useState} from "react"
+import React, {useContext, useEffect, useState} from "react"
 import "./timeline.css"
 import {Share} from "../share/Share";
 import axios from "axios";
 import {Post} from "../post/Post";
+import {AuthContext} from '../../state/AuthContext'
 
 export const TimeLine = ({username}) => {
     //状態管理
     const [posts, setPosts] = useState([]);
+    const {user} = useContext(AuthContext)
 
     useEffect(() => {
         const fetchPosts = async () => {
             const response = username
-                ? await axios.get(`/posts/profile/${username}`)
-                : await axios.get("/posts/timeline/633915134470348694751aac")
+                ? await axios.get(`/posts/profile/${username}`) //プロフィールの場合
+                : await axios.get(`/posts/timeline/${user._id}`) //ホームの場合
             setPosts(response.data)
         };
         fetchPosts();
-    }, [username])
+    }, [username, user._id])
 
     return (
         <div className="TimeLine">

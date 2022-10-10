d5c045418e5c3aa18dc952f00cb6b2bb98f49e22
Taishi1020 <tai.yakiman1020@gmail.com>
2022-10-10 22:50:33 +0900

    upd:　ユーザーが存在すればホームのページに遷移させ、ユーザーが存在しなければ、新規登録に遷移させる処理

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
             <Routes>
                 <Route path="/" element={user ? <Home/>: <Register/>}/>
                 <Route path="/login" element={user ? <Navigate to="/"/> : <Login/>}/>
-                <Route path="/register" element={<Register/>}/>
+                <Route path="/register" element={user ? <Navigate to="/"/> : <Register/>}/>
                 <Route path="/profile/:username" element={<Profile/>}/>
             </Routes>
         </Router>

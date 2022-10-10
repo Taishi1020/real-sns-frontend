159c8250126ccf78e3c3d8b7529cbb6a90dc2d93
Taishi1020 <tai.yakiman1020@gmail.com>
2022-10-10 14:44:55 +0900

    upd: ログインページにバリデーション設定する

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -11,8 +11,19 @@ export const Login = () => {
                 <div className="loginRight">
                     <div className="loginBox">
                         <p className="loginMsg">ログインはこちら</p>
-                        <input type="text" className="loginInput" placeholder="Eメール"/>
-                        <input type="text" className="loginInput" placeholder="パスワード"/>
+                        <input
+                            type="email"
+                            className="loginInput"
+                            placeholder="Eメール"
+                            required
+                        />
+                        <input
+                            type="password"
+                            className="loginInput"
+                            placeholder="パスワード"
+                            required
+                            minLength="6"
+                        />
                         <button className="loginButton">ログイン</button>
                         <span className="loginForgot">パスワードを忘れた方へ</span>
                         <button className="loginRegisterButton">アカウント作成</button>

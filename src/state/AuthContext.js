5552c1035203b18605e44a17acdb8f434502bc17
Taishi1020 <tai.yakiman1020@gmail.com>
2022-10-23 21:46:58 +0900

    upd:　ログインしたユーザーをローカルストレージに保存する機能を実装

--- src/state/AuthContext.js
+++ src/state/AuthContext.js
@@ -1,19 +1,20 @@
-import {createContext, useReducer} from 'react'
+import {createContext, useEffect, useReducer} from 'react'
 import AuthReducer from './AuthReducer'
 
 //最初のユーザー状態を定義
 const initalState = {
     // user: null, //userがnullだとブラウザを更新するたびに、新規登録画面に遷移してしまう
     user: {
-        _id: "632bbe0c58346bcee5b44edd",
-        username: "Taishi",
-        email: "Taishi@gmail.com",
-        password: "Taishi1999",
-        profilePicture: "/person/1.jpeg",
-        coverPicture: "",
-        followers: [],
-        followings: [],
-        isAdmin: false,
+        user: JSON.parse(localStorage.getITem("user")) || null
+        // _id: "632bbe0c58346bcee5b44edd",
+        // username: "Taishi",
+        // email: "Taishi@gmail.com",
+        // password: "Taishi1999",
+        // profilePicture: "/person/1.jpeg",
+        // coverPicture: "",
+        // followers: [],
+        // followings: [],
+        // isAdmin: false,
     },
     isFetching: false,
     error: false,
@@ -24,6 +25,10 @@ export const AuthContext = createContext(initalState);
 
 export const AuthContextProvider = ({children}) => {
     const [state, dispatch] = useReducer(AuthReducer, initalState);
+
+    useEffect(()=> {
+        localStorage.setItem("user", JSON.stringify(state.user))
+    }, [state.user])
     return (
         <AuthContext.Provider value={{
             user: state.user,
